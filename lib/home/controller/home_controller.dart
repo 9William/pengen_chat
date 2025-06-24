2d28b31fd933a1290fbf9bd9b44c7494703f9451
Aa Maulana <aamaulanabola10@gmail.com>
2025-06-24 14:29:54 +0700

    feat(realtime): add realtime feature and common setup

--- lib/home/controller/home_controller.dart
+++ lib/home/controller/home_controller.dart
@@ -1,51 +1,26 @@
 import 'package:get/get.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:pengen_chat/common/config/supabase.dart';
 import '../models/thread_model.dart';
 
 class HomeController extends GetxController {
-  late SupabaseClient supabase;
-
   var threads = <ThreadModel>[].obs;
-  late RealtimeChannel channel;
 
   @override
-  Future<void> onInit() async {
+  void onInit() {
     super.onInit();
-
-    await Supabase.initialize(
-      url: 'https://hyiyoxagxpanbpwfvhwj.supabase.co',
-      anonKey:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5aXlveGFneHBhbmJwd2Z2aHdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2NzA5MjUsImV4cCI6MjA2NjI0NjkyNX0.DlLd9iiY0BPiL3rfZj4fyQHtQvJRWhoSKlSd9AJ2j2E',
-    );
-    supabase = Supabase.instance.client;
-
-    fetchThreads();
-    setupRealtime();
+    listenToThreadStream();
   }
 
-  void fetchThreads() async {
-    final res = await supabase
+  void listenToThreadStream() {
+    SupabaseConfig.instance.supabaseClient
         .from('threads')
-        .select('id, title, like_count')
-        .order('created_at', ascending: false);
-
-    threads.value = (res as List)
-        .map((e) => ThreadModel.fromMap(e as Map<String, dynamic>))
-        .toList();
-  }
-
-  void setupRealtime() {
-    print("haloo");
-    supabase.from('threads').stream(primaryKey: ['id']).listen((
-      List<Map<String, dynamic>> data,
-    ) {
-      print("Keluar data $data");
-    });
-  }
-
-  @override
-  void onClose() {
-    supabase.removeChannel(channel);
-    super.onClose();
+        .stream(primaryKey: ['id'])
+        .order('created_at', ascending: false)
+        .map((data) => data
+            .map((e) => ThreadModel.fromJson(e))
+            .toList())
+        .listen((newList) {
+          threads.value = newList;
+        });
   }
 }
--- lib/home/view/home_view.dart
+++ lib/home/view/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:pengen_chat/common/utils/date_utils.dart';
 import 'package:pengen_chat/home/controller/home_controller.dart';
 
 class HomeView extends StatefulWidget {
@@ -10,9 +11,32 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final HomeController homeController = Get.put(HomeController());
+
   @override
   Widget build(BuildContext context) {
-    Get.put(HomeController());
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Threads")),
+      body: Obx(() {
+        final threads = homeController.threads;
+
+        if (threads.isEmpty) {
+          return const Center(child: Text("Belum ada thread bro 😴"));
+        }
+
+        return ListView.builder(
+          itemCount: threads.length,
+          itemBuilder: (context, index) {
+            final thread = threads[index];
+            return ListTile(
+              title: Text(thread.title ?? ""),
+              subtitle: Text(
+                DateUtil.timeAgoFromString(thread.createdAt ?? ""),
+              ),
+            );
+          },
+        );
+      }),
+    );
   }
 }
